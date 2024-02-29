fdab8e27e2e29d37363c69dabd1d51bd65ee8172
Сырчин Саша <cfif.3.v@gmail.com>
2024-02-29 11:15:05 +0300

    update log

--- lib/presentation/pages/chief/chief_page.dart
+++ lib/presentation/pages/chief/chief_page.dart
@@ -10,7 +10,6 @@ class ChiefPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const ContentChief();
-    
   }
 }
 
--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -37,6 +37,6 @@ ThemeData createTheme() {
               ),
             ),
           )
-        ),        
+        ),
         );
 }
